078c4be6c96f89f16833784ae8d8780eb1be9a20
gitlabdexter <gitlabdexter@gmail.com>
2025-08-14 19:07:54 +0800

    Update footer_details.js

--- src/components/footer_details.js
+++ src/components/footer_details.js
@@ -4,7 +4,7 @@ const FooterDetails = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <h2 className="footer-title">BLOODLINE DEV</h2>
+        <h2 className="footer-title">MTK DEV</h2>
         <p className="footer-copy">&copy; 2024 DexBuild. All rights reserved.</p>
       </div>
     </footer>
