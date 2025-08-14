50df424ed7000478b190ff44b665fcda252949c0
gitlabdexter <gitlabdexter@gmail.com>
2025-08-14 19:02:31 +0800

    Add files via upload

---
+++ src/contentData/content.js
@@ -0,0 +1,46 @@
+const productData = [
+    {
+      id: 1,
+      name: "Box 5g Speed Pro",
+      image:"https://i.ibb.co/1RF1Gfq/favicon.png",
+      productType: "openBackHeadphones",
+      description: ["OVPN", "UDP", "V2RAY"],
+      rating: 5,
+      timeLeft: 24,
+      appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/app.boxvpntunnel.dexbuild-DexBuild-V2-release.apk?inline=false" 
+       },
+    {
+      id: 2,
+      name: "SUMSAG ULTRA",
+      image:"https://i.ibb.co/1RF1Gfq/favicon.png",
+      productType: "openBackHeadphones",
+      description: ["OVPN", "UDP", "V2RAY", "SSH", "SLOWDNS"],
+      rating: 5,
+      timeLeft: 27,
+      appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/final-release.apk"
+   // },
+    //{
+   //   id: 3,
+   //   name: "IPHONE SPEED PRO",
+    //  image:
+    //    "https://i.ibb.co/z7pfLQc/icon-icon.png",
+   //   productType: "openBackHeadphones",
+   //   description: ["OVPN", "UDP", "V2RAY", "SSH", "SLOWDNS"],
+   //   rating: 5,
+    //  timeLeft: 24,
+   //   appLink: "https://panelvip.co.uk/iphone.apk"
+
+    }
+    // {
+    //   id: 4,
+    //   name: "SECURE TUNNEL",
+    //   image:
+    //     "https://i.postimg.cc/wMW7LWFC/keyhole-958517.png",
+    //   productType: "openBackHeadphones",
+    //   description: ["OVPN", "UDP", "V2RAY"],
+    //   rating: 5,
+    //   timeLeft: 24,
+    //   appLink: "https://tinyurl.com/StarLinkVPNnew-Apk"
+    // }
+  ];
+  export default productData;
---
+++ src/contentData/siteOption.js
@@ -0,0 +1,11 @@
+const siteOptionData = [
+    {
+      id: 1,
+      siteName: "BLOODLINE DEV",
+      image:
+        "https://i.ibb.co/1RF1Gfq/favicon.png",
+      description: "Experience a secure and reliable VPN service with enhanced privacy and performance.",
+      siteLink: "https://sumsag-ultra.site/"
+    }
+  ];
+  export default siteOptionData;
